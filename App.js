41d489ea8a60c68306537d66c887431b61c37413
EnzoVazz <evaz0711@gmail.com>
2026-03-16 11:49:58 -0300

    Configurando App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native/types_generated/index';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
